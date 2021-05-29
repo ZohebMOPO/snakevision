6b87f886cf5f3a73307c02f6517ac0d0d8aa587f
Zoheb Alli Khan <understandingzak@gmail.com>
2021-05-29 22:12:20 +0530

    All OpenCV code added

--- gameControl.py
+++ gameControl.py
@@ -83,26 +83,47 @@ while True:
             keyPressed = True
             keyPressed_lr = True
 
-    if len(contour_r):
-        c = max(contour_r, key=cv2.contourArea)
-        ((x2, y2), r2) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        left_centre = (int(M["m10"] / (M["m00"]+0.000001)),
-                       int(M["m01"] / (M["m00"] + 0.000001)))
-    if r > radius_of_circle:
-        cv2.circle(grabbed_Frame, (int(x2), int(y2)),
-                   int(r), (0, 255, 0), 2)
-        cv2.circle(grabbed_Frame, right_centre, 5, (0, 255, 0), -1)
+    if len(contour_r) > 0:
+        c2 = max(contour_r, key=cv2.contourArea)
+        ((x2, y2), r2) = cv2.minEnclosingCircle(c2)
+        M2 = cv2.moments(c2)
+        right_centre = (int(M2["m10"] / (M2["m00"]+0.000001)),
+                        int(M2["m01"] / (M2["m00"]+0.000001)))
+        right_centre = (right_centre[0]+width//2, right_centre[1])
 
-        if right_centre[1] < (height/2 - window_size//2):
-            cv2.putText(grabbed_Frame, 'LEFT', (20, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            pyautogui.press('left')
-            keyPressed = True
-            keyPressed_lr = True
-        elif left_centre[1] > (height/2 + window_size//2):
-            cv2.putText(grabbed_Frame, 'RIGHT', (20.50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            pyautogui.press('right')
-            keyPressed = True
-            keyPressed_lr = True
+        if r2 > radius_of_circle:
+            cv2.circle(grabbed_Frame, (int(x2)+width//2, int(y2)), int(r2),
+                       (0, 255, 0), 2)
+            cv2.circle(grabbed_Frame, right_centre, 5, (0, 255, 0), -1)
+            if right_centre[1] < (height//2 - window_size // 2):
+                cv2.putText(grabbed_Frame, 'UP', (200, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                pyautogui.press('up')
+                keyPressed = True
+                current_key.add(up)
+            elif right_centre[1] > (height//2 + window_size // 2):
+                cv2.putText(grabbed_Frame, 'DOWN', (200, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                pyautogui.press('down')
+                keyPressed = True
+                current_key.add(down)
+
+    grabbed_Frame_copy = grabbed_Frame.copy()
+
+    grabbed_Frame_copy = cv2.rectangle(grabbed_Frame_copy, (
+        0, height//2-window_size//2), (width, height//2 + window_size//2), (255, 0, 0), 2)
+
+    cv2.imshow("grabbed_frame", grabbed_Frame_copy)
+
+    if not keyPressed and current_key != 0:
+        for key in current_key:
+            ReleaseKey(key)
+            current_key = set()
+
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == ord('q'):
+        break
+
+video.stop()
+cv2.destroyAllWindows()
